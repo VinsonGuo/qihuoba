441c4ec68dac9a885b0c14e5b733e58277f17e54
vv123 <gzw19931217@qq.com>
2017-07-24 09:18:50 +0800

    迁移git

--- app/src/main/java/com/yjjr/yjfutures/ui/trade/AlipayTransferActivity.java
+++ app/src/main/java/com/yjjr/yjfutures/ui/trade/AlipayTransferActivity.java
@@ -12,6 +12,7 @@ import com.yjjr.yjfutures.ui.BaseActivity;
 import com.yjjr.yjfutures.utils.AlipayUtil;
 import com.yjjr.yjfutures.utils.ToastUtils;
 import com.yjjr.yjfutures.utils.http.HttpConfig;
+import com.yjjr.yjfutures.widget.HeaderView;
 
 public class AlipayTransferActivity extends BaseActivity {
 
@@ -23,6 +24,8 @@ public class AlipayTransferActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alipay_transfer);
+        HeaderView headerView = (HeaderView) findViewById(R.id.header_view);
+        headerView.bindActivity(mContext);
         Button btnConfirm = (Button) findViewById(R.id.btn_confirm);
         btnConfirm.setSelected(true);
         btnConfirm.setOnClickListener(new View.OnClickListener() {

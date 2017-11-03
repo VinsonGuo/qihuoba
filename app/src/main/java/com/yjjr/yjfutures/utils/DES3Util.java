a9e19895723ca0b961b2419802ec69f359e5f031
vv123 <gzw19931217@qq.com>
2017-11-03 14:28:06 +0800

    对分享的url编码

--- app/src/main/java/com/yjjr/yjfutures/utils/DES3Util.java
+++ app/src/main/java/com/yjjr/yjfutures/utils/DES3Util.java
@@ -1,5 +1,8 @@
 package com.yjjr.yjfutures.utils;
 
+import org.apache.commons.codec.binary.Base64;
+
+import java.net.URLEncoder;
 import java.security.Key;
 
 import javax.crypto.Cipher;
@@ -7,52 +10,48 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang3.StringUtils;
-
-
 
 /**
  * 3DES加密工具类
- * 
+ *
  * @author 感谢原始作者liufeng
  * @date 2014-7-9
  */
 public class DES3Util {
-	// 密钥
-	private final static String secretKey = "pR3t8Bp16BeDAMErGwnditpFhf9ZY4a1";
-	// 向量
-	private final static String iv = "12345678";
-	// 加解密统一使用的编码方式
-	private final static String encoding = "UTF-8";
-	/**
-	 * 3DES加密
-	 * 
-	 * @param plainText
-	 *            普通文本
-	 * @return
-	 * @throws Exception
-	 */
-	public static String encode(String plainText)  {
-		try {
-			return encode(plainText, secretKey);
-		}catch (Exception e) {
-			return null;
-		}
-	}
+    // 密钥
+    private final static String secretKey = "pR3t8Bp16BeDAMErGwnditpFhf9ZY4a1";
+    // 向量
+    private final static String iv = "12345678";
+    // 加解密统一使用的编码方式
+    private final static String encoding = "UTF-8";
 
-	public static String encode(String plainText, String secretKey)
-			throws Exception {
-		Key deskey = null;
-		DESedeKeySpec spec = new DESedeKeySpec(secretKey.getBytes());
-		SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("desede");
-		deskey = keyfactory.generateSecret(spec);
+    /**
+     * 3DES加密
+     *
+     * @param plainText 普通文本
+     * @return
+     * @throws Exception
+     */
+    public static String encode(String plainText) {
+        try {
+            return URLEncoder.encode(encode(plainText, secretKey), "UTF-8");
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
-		Cipher cipher = Cipher.getInstance("desede/CBC/PKCS5Padding");
-		IvParameterSpec ips = new IvParameterSpec(iv.getBytes());
-		cipher.init(Cipher.ENCRYPT_MODE, deskey,ips);
-		byte[] encryptData = cipher.doFinal(plainText.getBytes(encoding));
-		return new String(Base64.encodeBase64(encryptData));
-	}
+    public static String encode(String plainText, String secretKey)
+            throws Exception {
+        Key deskey = null;
+        DESedeKeySpec spec = new DESedeKeySpec(secretKey.getBytes());
+        SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("desede");
+        deskey = keyfactory.generateSecret(spec);
+
+        Cipher cipher = Cipher.getInstance("desede/CBC/PKCS5Padding");
+        IvParameterSpec ips = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, deskey, ips);
+        byte[] encryptData = cipher.doFinal(plainText.getBytes(encoding));
+        return new String(Base64.encodeBase64(encryptData));
+    }
 
 }
